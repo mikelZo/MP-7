e322c6074446397dd7d3529ea04fa0a0e157e307
Michael Zhou <mezhou2@illinois.edu>
2018-04-21 18:50:30 -0500

    Added all buttons except for secondary options, letters, and arrows. All buttons are regular buttons, but will eventually switch some to toggle buttons to test things.

--- app/src/main/java/edu/illinois/cs/cs125/mp_7/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp_7/MainActivity.java
@@ -2,12 +2,35 @@ package edu.illinois.cs.cs125.mp_7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "Mp7:Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /* Load the main layout for our activity */
         setContentView(R.layout.activity_main);
+
+        /*
+        Set up handlers for each button in our UI. These run when the buttons are clicked.
+         */
+        final Button second = findViewById(R.id.second);
+        second.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (final View v) {
+                Log.d(TAG, "second button clicked");
+                /* Insert swap tab code here */
+            }
+        });
+
     }
 }

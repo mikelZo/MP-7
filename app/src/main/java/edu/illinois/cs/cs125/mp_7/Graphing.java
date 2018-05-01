f6b8efaa47bc0e5c5f8323746077c471fec37583
Michael Zhou <mezhou2@illinois.edu>
2018-04-30 22:43:21 -0500

    Added graphing api to gradle and added graphing class.

---
+++ app/src/main/java/edu/illinois/cs/cs125/mp_7/Graphing.java
@@ -0,0 +1,26 @@
+package edu.illinois.cs.cs125.mp_7;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+
+public class Graphing {
+    private static double[] x = new double[5];
+    private static double[] y = new double[5];
+
+    public static void addX(final double value) {
+        for (int i = 0; i < x.length; i++) {
+            if (x[i] == 0) {
+                x[i] = value;
+            }
+        }
+    }
+
+    public static void addY(final double value) {
+        for (int i = 0; i < y.length; i++) {
+            if (y[i] == 0) {
+                y[i] = value;
+            }
+        }
+    }
+}
--- app/src/main/java/edu/illinois/cs/cs125/mp_7/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp_7/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.illinois.cs.cs125.mp_7;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
